cbe6b6eea1bf84c6fa172199e8435ff77948b02d
RaverGeek28100 <eder98100@gmail.com>
2021-06-25 09:58:57 -0500

    upd
    x222

--- assets/Js/main.js
+++ assets/Js/main.js
@@ -6,32 +6,75 @@ let Links = document.querySelectorAll(".close");
 //recorrer
 Links.forEach(function(link) {
 
-   //evento al dar click
+        //evento al dar click
 
-   link.addEventListener("click",function(ev){
-       ev.preventDefault();
-       let content= document.querySelector('.content');
-
-
-       //remove class
-       content.classList.remove("animate__fadeInDown");
-       content.classList.remove("animate__animated");
-
-       //add
-
-       content.classList.add("animate__fadeOutUp")
-       content.classList.add("animate__animated")
-
-       setTimeout(function() {
-           location.href="/LoquendoAlert";
-           
-       }, 601);
+        link.addEventListener("click",function(ev){
+                    ev.preventDefault();
+                    let content= document.querySelector('.content');
 
 
+                    //remove class
+                    content.classList.remove("animate__fadeInDown");
+                    content.classList.remove("animate__animated");
+
+                    //add
+
+                    content.classList.add("animate__fadeOutUp")
+                    content.classList.add("animate__animated")
+
+                    setTimeout(function() {
+                        //para server
+                        location.href="/LoquendoAlert";
+                        
+                    }, 601);
 
 
-    return false;
 
-   });
+
+            return false;
+
+        });
+
+  });
+
+
+
+
+  
+  //obtener
+let Bio = document.querySelectorAll(".biog");
+
+
+//recorrer
+Bio.forEach(function(link) {
+
+        //evento al dar click
+
+        link.addEventListener("click",function(ev){
+                    ev.preventDefault();
+                    let content= document.querySelector('.content');
+
+
+                    //remove class
+                    content.classList.remove("animate__fadeInDown");
+                    content.classList.remove("animate__animated");
+
+                    //add
+
+                    content.classList.add("animate__fadeOutUp")
+                    content.classList.add("animate__animated")
+
+                    setTimeout(function() {
+                        //para server
+                        location.href="/assets/html/Biografia.html";
+                        
+                    }, 601);
+
+
+
+
+            return false;
+
+        });
 
   });
